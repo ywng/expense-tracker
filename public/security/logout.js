da74951adfe905532d829f898034d01dfba790a5
Jason <victor.not.to.yield@gmail.com>
2018-02-04 02:01:43 -0500

    restructure...

--- public/security/logout.js
+++
@@ -1,9 +0,0 @@
-angular.module('app').component('logout', {
-  controller: function(auth, $location, $timeout) {
-    auth.$signOut()
-
-    $timeout(function() {
-      $location.path('/login');
-    }, 500);
-  }
-})
